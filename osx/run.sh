792342a1f2d7462b5ca507f534c7876be699f63e
Julian Richard Contreras <julian.r.contreras@gmail.com>
2022-06-13 11:35:05 -0400

    removed shell script

--- osx/run.sh
+++
@@ -1,5 +0,0 @@
-echo hello
-# source ~/.zshrc
-export PATH=/usr/local/bin/:$PATH
-/usr/local/bin/studio /Users/juliancontreras/code/android-sdk/android-sdk-base/src/main/java/com/braze/Braze.kt:371
-
